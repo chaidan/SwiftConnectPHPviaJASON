deac74e8c8386e516f20fff9aa98ccf463c0939a
student14 <chaidan@msn.com>
2017-06-15 14:01:09 +0700

    Initial Commit

---
+++ my Service/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  my Service
+//
+//  Created by Student14 on 6/15/2560 BE.
+//  Copyright © 2560 SNRU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Process connected Http connect via JASON
+        let urlPHP = "http://swiftcodingthai.com"
+        // Change String to url String
+        let myURL = URL(string: urlPHP)
+        let request = NSMutableURLRequest(url:myURL!)
+        // Create thread for URL
+        let task = URLSession.shared.dataTask(with: request as URLRequest){
+            // แสดงค่าที่รับจาก server ผ่าน response แสดง error ผ่น error
+            data, responde, error in
+            if error != nil {
+                print("Error-->\(error)")
+            } else {
+                if let unwrappedData = data {
+                    let dataString = NSString(data: unwrappedData, encoding:String.Encoding.utf8.rawValue)
+                    let strJSON=dataString as Any
+                    print("strJSON-->\(strJSON)")
+                
+                
+                }
+            }
+            
+            
+            }
+        
+
+        
+        task.resume()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    } // main method
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    } // didReceive
+
+
+} // main class
+
